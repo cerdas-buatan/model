b7e19aee6f276f6700b65e6a0675de5da8f8a7b8
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-10 00:06:46 +0700

    ubah yseq len

--- model1.py
+++ model1.py
@@ -95,7 +95,7 @@ def preprocess_data(questions, answers, xseq_len, yseq_len, num_words):
 
 # Set the parameters
 xseq_len = 20
-yseq_len = 25
+yseq_len = 23
 num_words = 10000
 emb_dim = 128
 num_layers = 1
