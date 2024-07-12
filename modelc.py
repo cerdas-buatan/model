a3390cb5ed0be221ca34c793bd85306a0bc20291
Ardiva <ardiva.diva30@gmail.com>
2024-07-12 18:05:11 +0700

    mengubah training argument

--- modelc.py
+++ modelc.py
@@ -20,9 +20,11 @@ train_data = train_data.apply(tokenize_function)
 
 training_args = TrainingArguments(
     output_dir="./results",
-    num_train_epochs=3,
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
+    num_train_epochs=5,
+    #per_device_train_batch_size=10,
+    #per_device_eval_batch_size=10,
+    per_device_train_batch_size=10,
+    per_device_eval_batch_size=10,
     warmup_steps=500,
     weight_decay=0.01,
     logging_dir="./logs",
