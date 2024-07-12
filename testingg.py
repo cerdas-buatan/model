060886603e5025e5f567a40610760dda564fa8e6
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-12 17:13:12 +0700

    ubah max length testing

--- testingg.py
+++ testingg.py
@@ -2,20 +2,20 @@ import tensorflow as tf
 from transformers import TFT5ForConditionalGeneration, T5Tokenizer
 
 # Load the trained model and tokenizer
-model_path = 't5_text_model'
-model = TFT5ForConditionalGeneration.from_pretrained(model_path) 
-tokenizer = T5Tokenizer.from_pretrained(model_path)   
-  
+model_path = 't5_text_to_text_model'
+model = TFT5ForConditionalGeneration.from_pretrained(model_path)
+tokenizer = T5Tokenizer.from_pretrained(model_path)
+
 # Function to generate text from input
 def generate_text(input_text):
     try:  
         # Tokenize the input text
-        inputs = tokenizer.encode_plus(input_text, return_tensors='tf', add_special_tokens=True, max_length=64, padding='max_length', truncation=True)
+        inputs = tokenizer.encode_plus(input_text, return_tensors='tf', add_special_tokens=True, max_length=20, padding='max_length', truncation=True)
         input_ids = inputs['input_ids']  
         attention_mask = inputs['attention_mask']  
 
         # Generate text from the model
-        outputs = model.generate(input_ids, attention_mask=attention_mask, max_length=20, num_beams=2, early_stopping=True)    
+        outputs = model.generate(input_ids, attention_mask=attention_mask, max_length=20, num_beams=2, early_stopping=True)
  
         # Decode the generated output  
         output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)  
