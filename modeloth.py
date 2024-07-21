d963fa4d10798428cd74f9a85c101da852566228
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-21 20:31:45 +0700

    add minus one

--- modeloth.py
+++ modeloth.py
@@ -103,3 +103,10 @@ tokenizer = T5Tokenizer.from_pretrained('t5-small')
 input_ids = []
 attention_masks = []
 labels = []
+
+for index, row in df.iterrows():
+    encoded_input = tokenizer.encode_plus(row['question'], add_special_tokens=True, max_length=64, padding='max_length', return_attention_mask=True, truncation=True)
+    encoded_output = tokenizer.encode_plus(row['answer'], add_special_tokens=True, max_length=64, padding='max_length', return_attention_mask=True, truncation=True)
+
+    input_ids.append(encoded_input['input_ids'])
+    attention_masks.append(encoded_input['attention_mask']) 
