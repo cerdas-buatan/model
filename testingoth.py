c98c67afddaf6b402cc90ade066e409124a491e6
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-21 20:38:25 +0700

    Menghasilkan teks

--- testingoth.py
+++ testingoth.py
@@ -42,5 +42,10 @@ while True:
     if input_text.lower() == 'exit':
         break
 
+        # Menghasilkan teks jawaban berdasarkan input
+    generated_text = generate_text(input_text)
+    print("Jawaban dari model:")
+    print(generated_text)
+
 
 
