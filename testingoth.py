2faafae9399881fc2c2080641647ffb9f1cd6e06
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-21 22:07:24 +0700

    update model path

--- testingoth.py
+++ testingoth.py
@@ -3,16 +3,16 @@ from transformers import TFT5ForConditionalGeneration, T5Tokenizer
 import pandas as pd
 
 # load model dan tokenizer yang telah dilatih
-model_path = 't5_text_to_text_model'
+model_path = 't5_text_to_text_model' 
 model = TFT5ForConditionalGeneration.from_pretrained(model_path)
 tokenizer = T5Tokenizer.from_pretrained(model_path)
 
 # Inisialisasi daftar untuk menyimpan pertanyaan, jawaban, dan akurasi
-data = []
+data = [] 
 
 # Fungsi menghasilkan teks dari input
-def generate_text(input_text):
-    try:
+def generate_text(input_text): 
+    try: 
         # Tokenisasi input
         inputs = tokenizer.encode_plus(input_text, return_tensors='tf', add_special_tokens=True, max_length=64, padding='max_length', truncation=True) 
         input_ids = inputs['input_ids'] 
