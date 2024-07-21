cadc4dee8c45e20e895d6dde29f219b70d6d70b6
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2024-07-21 21:50:06 +0700

    memperbaiki training

--- modeloth.py
+++ modeloth.py
@@ -60,3 +60,8 @@ model.compile(optimizer=optimizer, loss=compute_loss)
 # Definisikan dataset untuk training
 dataset = tf.data.Dataset.from_tensor_slices(({"input_ids": input_ids, "attention_mask": attention_masks}, labels))
 dataset = dataset.shuffle(buffer_size=1024).batch(16)
+
+# Training loop
+epochs = 3
+for epoch in range(epochs):
+    print(f'Epoch {epoch + 1}/{epochs}')
