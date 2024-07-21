c29ec49873bc8344203cacfc0b7123c416b7e761
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-21 22:35:39 +0700

    update generate text

--- testingg.py
+++ testingg.py
@@ -7,35 +7,27 @@ model_path = 't5_text_to_text_model'
 model = TFT5ForConditionalGeneration.from_pretrained(model_path)
 tokenizer = T5Tokenizer.from_pretrained(model_path)
 
-def load_and_preprocess_data(filepath):
-    """
-    Load and preprocess the dataset.
-    """
-    with open(filepath, 'r', encoding='utf-8') as file:
-        reader = csv.reader(file, delimiter='|')
-        filtered_rows = [row for row in reader if len(row) == 2 and row[0].strip() != "" and row[1].strip() != ""]
+# Inisialisasi daftar untuk menyimpan pertanyaan, jawaban, dan akurasi
+data = []
 
-    df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
-    label_encoder = LabelEncoder()
-    label_encoder.fit(df['answer'])
-    
-    return df, label_encoder
-
-def generate_text(input_text, tokenizer, model):
-    """
-    Generate text from the input using the T5 model.
-    """
+# Fungsi menghasilkan teks dari input
+def generate_text(input_text):
     try:
-        inputs = tokenizer.encode_plus(input_text, return_tensors='tf', add_special_tokens=True, max_length=50, padding='max_length', truncation=True)
+        # Tokenisasi input
+        inputs = tokenizer.encode_plus(input_text, return_tensors='tf', add_special_tokens=True, max_length=64, padding='max_length', truncation=True)
         input_ids = inputs['input_ids']
         attention_mask = inputs['attention_mask']
-        
-        outputs = model.generate(input_ids, attention_mask=attention_mask, max_length=50, num_beams=2, early_stopping=True)
+
+        # Menghasilkan teks dari model
+        outputs = model.generate(input_ids, attention_mask=attention_mask, max_length=64, num_beams=4, early_stopping=True)
+
+        # dekode output yang dihasilkan
         output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
         return output_text
     except Exception as e:
-        return f"Error in generating text: {str(e)}"
+        return f"Error dalam menghasilkan teks: {str(e)}"
+
 
 def main():
     model_path = 't5_text_to_text_model'
