273ad1915a28c2c45389ba5d43a7f074757c7408
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-21 20:29:36 +0700

    menghapus kembali

--- testingoth.py
+++ testingoth.py
@@ -34,16 +34,4 @@ def evaluate_accuracy(input_text, generated_text):
     # Dalam kasus nyata, ini harus membandingkan teks yang dihasilkan dengan jawaban yang diharapkan.
     return "Akurasi Placeholder"
 
-# Penggunaan
-while True:
-    input_text = input("Masukkan pertanyaan Anda (atau ketik 'exit' untuk keluar): ")
-
-    if input_text.lower() == 'exit':
-        break
-
-    # Menghasilkan teks jawaban berdasarkan input
-    generated_text = generate_text(input_text)
-    print("Jawaban dari model:")
-    print(generated_text)
-
 
