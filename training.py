4257cabd6878d11e73e7687b4fbb784758a09347
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-22 22:15:44 +0700

    update model

--- training.py
+++ training.py
@@ -1,3 +1,8 @@
+# # import library yang di butuhkan
+# import tensorflow as tf
+# from transformers import TFT5ForConditionalGeneration, T5Tokenizer
+# import pandas as pd 
+
 # import library yang di butuhkan
 import tensorflow as tf
 from transformers import TFT5ForConditionalGeneration, T5Tokenizer
