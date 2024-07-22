a70d22fcae0af4ea23eb4c6fbf49e2df42d46b9f
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-22 22:33:25 +0700

    add save testing to excel

--- testing2.py
+++ testing2.py
@@ -50,4 +50,10 @@ while True:
     # Mengevaluasi akurasi
     accuracy = evaluate_accuracy(input_text, generated_text)
 
+    # Menyimpan pertanyaan, jawaban, dan akurasi ke dalam daftar data
+    data.append({"Pertanyaan": input_text, "Jawaban": generated_text, "Akurasi": accuracy})
 
+# Menyimpan data testing ke file Excel
+df = pd.DataFrame(data)
+df.to_excel("hasil_test.xlsx", index=False)
+print("Data berhasil disimpan ke hasil_test.xlsx")
