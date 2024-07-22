5895fb1eaced2519aaf8111bfdd5c27b3f3cfa18
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-22 22:27:11 +0700

    add optimizer

--- training2.py
+++ training2.py
@@ -47,3 +47,20 @@ labels = tf.constant(labels)
 # Muat model GPT-2
 model = TFGPT2LMHeadModel.from_pretrained('gpt2')
 
+# Definisikan optimizer
+optimizer = tf.keras.optimizers.Adam(learning_rate=5e-5)
+
+# Definisikan fungsi loss custom
+def compute_loss(labels, logits):
+    return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+# Definisikan fungsi akurasi custom
+def masked_accuracy(y_true, y_pred):
+    y_true = tf.cast(tf.reshape(y_true, (-1,)), tf.int64)
+    y_pred = tf.cast(tf.argmax(y_pred, axis=-1), tf.int64)
+    y_pred = tf.reshape(y_pred, (-1,))  # Pastikan y_pred diubah bentuknya agar sesuai dengan y_true
+    accuracy = tf.equal(y_true, y_pred)
+    mask = tf.cast(tf.not_equal(y_true, tokenizer.pad_token_id), tf.float32)  # Abaikan token padding
+    accuracy = tf.cast(accuracy, tf.float32) * mask
+    return tf.reduce_sum(accuracy) / tf.reduce_sum(mask)
+
