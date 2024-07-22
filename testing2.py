0a370575af270aa2bcd39f75834efe6b7933aa63
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-22 22:30:32 +0700

    load model dan tokenizer

--- testing2.py
+++ testing2.py
@@ -3,3 +3,11 @@ import tensorflow as tf
 from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
 import pandas as pd
 
+# load model dan tokenizer yang telah dilatih
+model_path = 'gpt2_model'
+model = TFGPT2LMHeadModel.from_pretrained(model_path)
+tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+
+# Inisialisasi daftar untuk menyimpan pertanyaan, jawaban, dan akurasi
+data = []
+
