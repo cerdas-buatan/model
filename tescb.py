a4aa26e9e8a396568ead96735a9740d303136003
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-24 18:56:17 +0700

    inisialisasi

--- tescb.py
+++ tescb.py
@@ -6,3 +6,7 @@ import pandas as pd
 model_path = 't5_text_to_text_model'
 model = TFT5ForConditionalGeneration.from_pretrained(model_path)
 tokenizer = T5Tokenizer.from_pretrained(model_path)
+
+
+# Inisialisasi daftar untuk menyimpan pertanyaan, jawaban, dan akurasi
+data = []
