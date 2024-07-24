c6edc17645bd9fb41e99ebe65e691a2dd53aba69
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-07-24 18:56:32 +0700

    Tokenisasi input

--- tescb.py
+++ tescb.py
@@ -10,3 +10,11 @@ tokenizer = T5Tokenizer.from_pretrained(model_path)
 
 # Inisialisasi daftar untuk menyimpan pertanyaan, jawaban, dan akurasi
 data = []
+
+# Fungsi menghasilkan teks dari input
+def generate_text(input_text):
+    try:
+        # Tokenisasi input
+        inputs = tokenizer.encode_plus(input_text, return_tensors='tf', add_special_tokens=True, max_length=64, padding='max_length', truncation=True)
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
