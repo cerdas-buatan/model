189a639ebd2c526b6e3084fedbf8245925b6023a
Ardiva <Ardiva.diva30@gmail.com>
2024-07-24 20:14:48 +0800

    delete unused func

--- modelc.py
+++ modelc.py
@@ -40,17 +40,14 @@ class Seq2Seq:
         decoder_outputs, _, _ = decoder_lstm(dec_emb, initial_state=encoder_states)
         decoder_outputs = Dense(self.vocab_size, activation='softmax', name='decoder_dense')(decoder_outputs)
 
-
         # Define and compile the model
         self.model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
         self.model.compile(optimizer=Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
 
     def train(self, X_train, y_train, batch_size, epochs):
         y_train = np.expand_dims(y_train, axis=-1)
         history = self.model.fit([X_train, y_train[:, :-1]], y_train[:, 1:], batch_size=batch_size, epochs=epochs, validation_split=0.1)
         return history
-
 
     def evaluate(self, X_test, y_test):
         y_test = np.expand_dims(y_test, axis=-1)
@@ -59,14 +56,12 @@ class Seq2Seq:
         print(f'Test Accuracy: {results[1]}')
         return results
 
-
 def preprocess_data(questions, answers, xseq_len, yseq_len, num_words):
     tokenizer = Tokenizer(num_words=num_words, oov_token='<OOV>')
     tokenizer.fit_on_texts(np.concatenate((questions, answers)))
     X = pad_sequences(tokenizer.texts_to_sequences(questions), maxlen=xseq_len, padding='post')
     y = pad_sequences(tokenizer.texts_to_sequences(answers), maxlen=yseq_len, padding='post')
     return X, y, tokenizer
-
 
 # Load and preprocess the dataset
 data = pd.read_csv('dataset_clean2.csv')
@@ -87,7 +82,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = Seq2Seq(xseq_len=xseq_len, yseq_len=yseq_len, vocab_size=num_words, emb_dim=emb_dim, num_layers=num_layers)
 history = model.train(X_train, y_train, batch_size=16, epochs=3)
 
-# Evaluate the model
 # Evaluate the model
 results = model.evaluate(X_test, y_test)
 
