cff5464e1129c390d92157b15207167694d21200
Ardiva <Ardiva.diva30@gmail.com>
2024-07-24 20:14:13 +0800

    def build

--- modelc.py
+++ modelc.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing.text import Tokenizer
+#from tensorflow.keras.layers import Embedding, LSTM, Dense, Input
+#from tensorflow.keras.models import Model
+#from tensorflow.keras.optimizers import Adam
+#from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 import os
