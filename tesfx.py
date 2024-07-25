53aacec3fc299b21684cb2dc6cd5579bc8bb82ba
Ardiva <Ardiva.diva30@gmail.com>
2024-07-25 20:15:26 +0800

    edit dekode output

--- tesfx.py
+++ tesfx.py
@@ -18,14 +18,15 @@ data = []
 def generate_text(input_text):
     try:
         # Tokenisasi input
-        # Tokenisasi input
         inputs = tokenizer.encode_plus(input_text, return_tensors='tf', add_special_tokens=True, max_length=64, padding='max_length', truncation=True)
         input_ids = inputs['input_ids']
         attention_mask = inputs['attention_mask']
 
         # Menghasilkan teks dari model
+        # Menghasilkan teks dari model
         outputs = model.generate(input_ids, attention_mask=attention_mask, max_length=64, num_beams=4, early_stopping=True)
 
+        # dekode output yang dihasilkan
         # dekode output yang dihasilkan
         output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
