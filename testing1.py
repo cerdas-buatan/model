85f0c4c0a01dc4b3c6ea4f38806984cdb0504421
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-25 20:44:37 +0700

    add load model

--- testing1.py
+++ testing1.py
@@ -2,4 +2,11 @@ import tensorflow as tf
 import joblib
 import os
 
+# Define folder to load model and other files
+save_dir = 'saved_model'
+
+# Load model, vectorizer, and label encoder
+model = tf.keras.models.load_model(os.path.join(save_dir, 'nn_model.h5'))
+vectorizer = joblib.load(os.path.join(save_dir, 'vectorizer.pkl'))
+label_encoder = joblib.load(os.path.join(save_dir, 'label_encoder.pkl'))
 
