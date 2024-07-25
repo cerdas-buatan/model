9e73d58ae6c4682a57806308fccfbb991f9201b4
MSyahidAlFajri <masyahida963@gmail.com>
2024-07-25 20:37:45 +0700

    update model

--- bow.py
+++ bow.py
@@ -12,7 +12,7 @@ rows = []
 # Read and clean dataset, handling any anomalies
 with open('dataset_clean2.csv', 'r', encoding='utf-8') as file:
     for line_number, line in enumerate(file):
-        # Pisahkan baris berdasarkan '|' dan tangani baris yang tidak terduga
+        # Pisahkan baris berdasarkan '|'
         parts = line.strip().split('|')
         if len(parts) == 2:  # Hanya memproses baris dengan tepat dua bagian
             rows.append(parts)
@@ -26,17 +26,11 @@ df.dropna(inplace=True)
 # Inisialisasi CountVectorizer
 vectorizer = CountVectorizer()
 
-# Gabungkan kolom 'question' dan 'answer' untuk representasi BoW
-combined_texts = df['question'] + " " + df['answer']
-
 # Fit dan transform teks ke BoW
-bow_matrix = vectorizer.fit_transform(combined_texts)
+X_bow = vectorizer.fit_transform(df['question'])
 
-# Konversi BoW matrix ke DataFrame
-bow_df = pd.DataFrame(bow_matrix.toarray(), columns=vectorizer.get_feature_names_out())
+# Menggunakan kolom 'answer' sebagai label
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(df['answer'])
 
-# Tampilkan beberapa baris dari DataFrame BoW
-print(bow_df.head())
 
-# Simpan BoW dataframe ke file CSV jika diperlukan
-bow_df.to_csv('bow_representation.csv', index=False)
