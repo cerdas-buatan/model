c05d21001c6bce240d1d8bcf7a7f809e9029e369
Ardiva <Ardiva.diva30@gmail.com>
2024-07-25 19:46:37 +0800

    edit load model

--- tesfx.py
+++ tesfx.py
@@ -2,6 +2,10 @@ import tensorflow as tf
 from transformers import TFT5ForConditionalGeneration, T5Tokenizer
 import pandas as pd
 
+#import tensorflow as tf
+#from transformers import TFT5ForConditionalGeneration, T5Tokenizer
+#import pandas as pd
+
 # load model dan tokenizer yang telah dilatih
 model_path = 't5_text_to_text_model'
 model = TFT5ForConditionalGeneration.from_pretrained(model_path)
