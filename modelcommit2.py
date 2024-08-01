535652675ed49da3c46ea6e4ceb1bc981ea58713
mfaisal-Ash <sidiqfaisal30@gmail.com>
2024-08-01 22:35:33 +0700

    hpus library unsed

--- modelcommit2.py
+++ modelcommit2.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import joblib
